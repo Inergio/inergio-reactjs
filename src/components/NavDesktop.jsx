16fb925309500855950178798b1d9f5388974a1a
Tomas <tomas.britos@web-experto.com.ar>
2024-01-25 21:30:01 -0300

    add wtsp icon in contact btn

--- src/components/NavDesktop.jsx
+++ src/components/NavDesktop.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Icon } from './icons/Icon';
 import { Instagram } from './icons/Instagram';
+import { Whatsapp } from './icons/Whatsapp';
 
 function NavDesktop() {
   return (
@@ -15,7 +16,8 @@ function NavDesktop() {
         Novedades
       </a>
       <a className="c-btn c-btn-contained-primary not-pill" href="/contacto">
-        Contacto
+        <Icon iconSvg={<Whatsapp />} />
+        Contactar
       </a>
       <a className="c-btn c-btn-text-secondary not-pill" href="https://www.instagram.com/inergio.ingenieria/?hl=es-la" target="_blank">
         <Icon iconSvg={<Instagram />} />
