bd36cbb757ea7002202577860542a003e6b8972a
Tomas <tomas.britos@web-experto.com.ar>
2024-01-25 21:32:41 -0300

    change icon x wtsp

--- src/components/NavDesktop.jsx
+++ src/components/NavDesktop.jsx
@@ -14,7 +14,7 @@ function NavDesktop() {
         Novedades
       </Link>
       <Link className="c-btn c-btn-contained-primary not-pill"  to="https://wa.me/c/5493416590047">
-       <Icon iconSvg={<Instagram />} />
+       <Icon iconSvg={<Whatsapp />} />
         Contactar
       </Link>
       <Link className="c-btn c-btn-text-secondary not-pill" to="https://www.instagram.com/inergio.ingenieria/?hl=es-la" target="_blank" rel="noreferrer">
