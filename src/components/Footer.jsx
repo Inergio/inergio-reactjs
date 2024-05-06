efa79075eedd753a0946ba3b00f9a3b565d7533d
Tomas <tomas.britos@web-experto.com.ar>
2024-05-06 20:57:08 -0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
     <footer>
-      <div className='copyright'>
-        © Inergio. All rights reserved.
-      </div>
+      <div className="copyright">© Inergio. All rights reserved.</div>
       <div className="creators">
-        Design: 
-        <a href="https://www.linkedin.com/in/tomas-britos/" target="_blank" rel="noreferrer">Tomás Britos</a>|
-        <a href="https://www.linkedin.com/in/carlos-britos-398aa620a/" target="_blank" rel="noreferrer">Carlos Britos</a>|
-        <a href="https://www.linkedin.com/in/bruno-rios-arg/" target="_blank" rel="noreferrer">Bruno Ríos</a>
+        Design:
+        <a
+          href="https://www.linkedin.com/in/tomas-britos/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tomás Britos
+        </a>
+        |
+        <a
+          href="https://www.linkedin.com/in/carlos-britos-398aa620a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Carlos Britos
+        </a>
       </div>
     </footer>
-  )
+  );
 }
 
-export { Footer }
+export { Footer };
